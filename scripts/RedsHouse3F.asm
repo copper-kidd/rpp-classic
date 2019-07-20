a451783d51958c9f6c10f0657c25100ecb78b1ec
Copper Kidd <outchy@gmail.com>
2019-07-19 23:01:45 -0400

    Added sound effect to obtaining nugget

--- scripts/RedsHouse3F.asm
+++ scripts/RedsHouse3F.asm
@@ -1,32 +1,27 @@
 RedsHouse3FScript:
-	call EnableAutoTextBoxDrawing
-	ld hl,RedsHouse3FScriptPointers
-	jp CallFunctionInTable
-
-RedsHouse3FScriptPointers:
-	dw RedsHouse3FScript0
-
-RedsHouse3FScript0:
-	ret
+	jp EnableAutoTextBoxDrawing
 
 RedsHouse3FTextPointers:
 	dw RedsHouse3FPokeball
-
+	dw FoundItemText2
+	
 RedsHouse3FPokeball:
 	TX_ASM
 	lb bc, NUGGET, 1
 	call GiveItem
 	jr nc, .BagFull
-	SetEvent EVENT_GOT_NUGGET
 	ld hl, .NuggetText
 	call PrintText
-	jp TextScriptEnd
-
+	ret
+	
 .BagFull
-	ld hl, Route24Text_51521
-	call PrintText
-	jp TextScriptEnd
-
+	ld hl, NoMoreRoomForItemText
+	db "@"
+	
 .NuggetText
 	TX_FAR _RedsHouse3FNuggetText
+	db "@"
+	
+FoundItemText2:
+	TX_SFX_ITEM_1
 	db "@"
