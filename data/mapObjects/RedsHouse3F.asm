755b10e83986a84048fd905a0c749c1a780a51c9
Copper Kidd <outchy@gmail.com>
2019-07-20 00:30:46 -0400

    Fixed warp_to and incorrect warp comment

--- data/mapObjects/RedsHouse3F.asm
+++ data/mapObjects/RedsHouse3F.asm
@@ -2,7 +2,7 @@ RedsHouse3FObject:
 	db $0 ; border block
 
 	db 1 ; warps
-	warp  3,  0, 1, REDS_HOUSE_2F ; The "1" makes Red come out of 3F stairs
+	warp  3,  0, 1, REDS_HOUSE_2F ; The "1" tells which warp-to point on the other map you come out of
 
 	db 0 ; signs
 
@@ -10,4 +10,4 @@ RedsHouse3FObject:
 	object SPRITE_BALL,  1,  5, STAY, NONE, 1, NUGGET
 
 	; warp-to
-	warp_to  3,  0, REDS_HOUSE_2F_WIDTH ; Where Red starts on 3F
+	warp_to  3,  0, REDS_HOUSE_3F_WIDTH ; Where Red starts on 3F
